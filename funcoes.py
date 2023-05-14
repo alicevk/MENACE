ebf247649f3a481374047b57603304960e6cf46e
Daniel R. Cassar <daniel.r.cassar@gmail.com>
2023-05-14 09:22:24 -0300

    nova função para computar todos os jogos

--- funcoes.py
+++ funcoes.py
@@ -3,29 +3,31 @@ import itertools
 from functools import partial
 
 
-# todas as operações para gerar configurações simétricas. Não sei se está
-# correto e completo!
 ALL_SYMMETRY_OP = {
     "id": lambda x: x,
     "rot90": np.rot90,
-    "rot180": partial(np.rot90, k=2),
-    # "rot270": partial(np.rot90, k=3),  # precisa desse?
     "flipv": np.fliplr,
     "fliph": np.flipud,
     "flipds": lambda x: np.fliplr(np.rot90(x)),
     "flipdp": lambda x: np.flipud(np.rot90(x)),
+
+    # Operações desnecessárias (apenas para registro)
+    # "rot180": partial(np.rot90, k=2),
+    # "rot270": partial(np.rot90, k=3),
 }
 
 # Operaçãos inversas
 ALL_SYMMETRY_OP_INV = {
     "id": lambda x: x,
     "rot90": partial(np.rot90, k=-1),
-    "rot180": partial(np.rot90, k=-2),
-    # "rot270": partial(np.rot90, k=-3),
     "flipv": np.fliplr,
     "fliph": np.flipud,
     "flipds": lambda x: np.fliplr(np.rot90(x)),
     "flipdp": lambda x: np.flipud(np.rot90(x)),
+
+    # Operações desnecessárias (apenas para registro)
+    # "rot180": partial(np.rot90, k=-2),
+    # "rot270": partial(np.rot90, k=-3),
 }
 
 
@@ -202,28 +204,57 @@ if __name__ == "__main__":
     print(jogo.symmetry_map())
 
 
-# listando todos os jogos únicos onde o jogador 1 deve fazer um movimento
-# (assumindo que o jogador 1 começa o jogo)
-# onde o jogador 1 tem mais que uma posição para jogar
-if False:
+def lista_todos_jogos(vez_de_quem_jogar="1"):
+    """Lista todos os jogos possíveis no jogo da velha.
+
+    Lista apenas jogos onde mais de uma escolha pode ser feita.
+
+    Args:
+      vez_de_quem_jogar : str
+        `1`: jogador 1
+        `2`: jogador 2
+        `12`: tanto faz
+
+    Condições:
+      + Jogador 1 é quem começa a jogar
+      + Jogos já ganhos não são listados
+    """
+
+    if vez_de_quem_jogar == "1":
+        diff = [0]
+    elif vez_de_quem_jogar == "2":
+        diff = [1]
+    else:
+        diff = [0, 1]
+
     games = set(
         [
             Configuracao(jogo).get_symmetry_id()
             for jogo in itertools.product([0, 1, 2], repeat=9)
-            if jogo.count(1) - jogo.count(2) == 0
+            if jogo.count(1) - jogo.count(2) in diff
             and not (
                 Configuracao(jogo).check_vitoria_1()
                 or Configuracao(jogo).check_vitoria_2()
             )
-            and not jogo.count(0) == 1
+            and not jogo.count(0) in [0, 1]
         ]
     )
 
     games = list(sorted(games))
-    print("Temos um total de ", len(games), "configurações")
 
-    # exemplos
-    print()
-    for g in games[-30:]:
-        print(Configuracao(g))
-        print()
+    return games
+
+games1 = lista_todos_jogos("1")
+print("Temos um total de", len(games1), "configurações")
+
+games2 = lista_todos_jogos("2")
+print("Temos um total de", len(games2), "configurações")
+
+games3 = lista_todos_jogos("12")
+print("Temos um total de", len(games3), "configurações")
+
+# # exemplos
+# print()
+# for g in games[-30:]:
+#     print(Configuracao(g))
+#     print()
